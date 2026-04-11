cabbfbcb7a720664333598967a4bb345c2019e10
test <test>
2026-04-11 18:41:13 +0200

    commands implentation start

--- includes/Commands.hpp
+++ includes/Commands.hpp
@@ -1,1 +1,28 @@
-// command parsing
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include "Server.hpp"
+#include "Client.hpp"
+
+class Commands {
+public:
+    void parseCommand(const std::string& command, std::vector<std::string>& args);
+    void execute(int fd, const std::string& command, Server* server);
+
+    void PASS(int fd, const std::vector<std::string>& args, Server* server);
+    void NICK(int fd, const std::vector<std::string>& args, Server* server);
+    void USER(int fd, const std::vector<std::string>& args, Server* server);
+    void PING(int fd, const std::vector<std::string>& args, Server* server);
+    void QUIT(int fd, const std::vector<std::string>& args, Server* server);
+    void MODE(int fd, const std::vector<std::string>& args, Server* server);
+    void JOIN(int fd, const std::vector<std::string>& args, Server* server);
+    void PART(int fd, const std::vector<std::string>& args, Server* server);
+    void PRIVMSG(int fd, const std::vector<std::string>& args, Server* server);
+    void TOPIC(int fd, const std::vector<std::string>& args, Server* server);
+    void KICK(int fd, const std::vector<std::string>& args, Server* server);
+    void INVITE(int fd, const std::vector<std::string>& args, Server* server);
+};
+
+
+
+#endif
--- includes/Server.hpp
+++ includes/Server.hpp
@@ -9,6 +9,7 @@
 #include <cstring>
 #include <cstdlib>
 #include <cerrno>
+#include <sstream>
 
 #include <unistd.h>
 #include <fcntl.h>
